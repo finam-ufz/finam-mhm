f7197081e20e6281cef90a7b269e92dd1c984265
Sebastian Müller <mueller.seb@posteo.de>
2023-09-01 16:03:08 +0200

    update example

--- examples/simple_viewer.py
+++ examples/simple_viewer.py
@@ -1,22 +1,29 @@
 """
 Simple coupling setup using live view modules.
 """
+import shutil
 from datetime import datetime, timedelta
+from pathlib import Path
 
 import finam as fm
-import finam_mhm as fm_mhm
-import finam_plot as fm_plt
 import finam_netcdf as fm_nc
+import finam_plot as fm_plt
+from mhm import download_test
+
+import finam_mhm as fm_mhm
 
 start_date = datetime(1990, 1, 1)
 day = timedelta(days=1)
+here = Path(__file__).parent
+test_domain = here / "test_domain"
+shutil.rmtree(test_domain, ignore_errors=True)
 
-mhm = fm_mhm.MHM(cwd="../../MHM/mhm/test_domain")
-runoff_viewer = fm_plt.ImagePlot(vmin=0.0, vmax=650)
-
+download_test(path=test_domain)
+mhm = fm_mhm.MHM(cwd=test_domain)
+runoff_viewer = fm_plt.ImagePlot(vmin=0.0, vmax=650, update_interval=24)
 # netcdf writing files
 writer = fm_nc.NetCdfTimedWriter(
-    path="qmod.nc",
+    path=here / "qmod.nc",
     inputs={"QMOD": fm_nc.Layer(var="QMOD", xyz=("x", "y"))},
     time_var="time",
     start=start_date,
@@ -29,4 +36,4 @@ composition.initialize()
 mhm.outputs["L11_QMOD"] >> writer.inputs["QMOD"]
 mhm.outputs["L11_QMOD"] >> runoff_viewer.inputs["Grid"]
 
-composition.run(end_time=datetime(1992, 1, 1))
+composition.run(end_time=datetime(1991, 1, 1))
